7c307dfed72c600c79d6b7558bf8c340db69db63
Rolwen <n.rnolwen@gmail.com>
2022-11-03 16:26:56 +0100

    Modification du main.c afin d'inclure des lignes de codes permettant d'afficher la liste chainée du dictionnaire

--- main.c
+++ main.c
@@ -3,10 +3,30 @@
 //
 #include <stdio.h>
 #include "structures.h"
+#include "fonctions.h"
 
-int main(){
+int main()
+{
+    /*
     p_node test;
     test = createNode('a');
-    printf("%c",test->letter);
+    printf("%c",test->letter);*/
+
+    t_dictionarylist dictionary_list;
+    dictionary_list = Split_dictionary_in_linked_list();
+    p_dictionarycell temp = dictionary_list.head;
+    if(temp == NULL)
+    {
+        printf("list empty");
+    }
+    else
+    {
+        while(temp != NULL)
+        {
+            printf("%s %s %s %s",temp->type,temp->forme_flechie,temp->base_word,temp->troisieme_cate);
+            temp = temp->next;
+        }
+    }
     return 0;
 }
+
